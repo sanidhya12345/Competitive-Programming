23ac08f98fc6918424fc59b9b22f58e9956c770d
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2021-08-09 12:15:37 +0530

    Create ChefAndBulbInvention.java

---
+++ ChefAndBulbInvention.java
@@ -0,0 +1,37 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+        int testcases= sc.nextInt();
+        while(testcases!=0){
+            int n= sc.nextInt();
+            int p=sc.nextInt();
+            int k=sc.nextInt();
+            int count;
+            int g=p%k;
+            int y = ((n-1)/k)*k;
+            y=n-1-y;
+            if (y<g){
+                count=((n-1)/k)*g + (y+1);
+            }
+            else{
+                count=((n-1)/k + 1) * g;
+            }
+            for (int i = g; i <n ; i+=k) {
+                count+=1;
+                if (i==p){
+                    System.out.println(count);
+                }
+            }
+            testcases--;
+        }
+	}
+}
