648324bc808070131ac4c0636f553cce3e2faa49
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2021-12-05 16:51:28 +0530

    linked list generics implementation

---
+++ linkedListpractice/LinkedListGenerics.java
@@ -0,0 +1,60 @@
+package linkedListpractice;
+
+public class LinkedListGenerics<T>{
+      private ListNode<T> head=null;
+      public static class ListNode<T>{
+        private T data;
+        private ListNode<T> next;
+        public ListNode(T data){
+          this.data=data;
+          this.next=null;
+        }
+      }
+      public void addNode(T data){
+        ListNode<T> newNode=new ListNode<>(data);
+        if (head==null) {
+          head=newNode;
+        }
+        else{
+          ListNode<T> currentNode=head;
+          while (currentNode.next!=null) {
+              currentNode=currentNode.next;
+          }
+          currentNode.next=newNode;
+        }
+      }
+      public void deleteNode(){
+        if (head==null) {
+          System.out.println("list is empty...");
+        }
+        else{
+          ListNode<T> curNode=head;
+          while(curNode.next.next!=null){
+            curNode=curNode.next;
+          }
+          curNode.next=null;
+        }
+      }
+      public void traverseList(){
+        if (head==null) {
+          System.out.println("list is empty...");
+        }
+        else{
+          ListNode<T> curNode;
+          for (curNode=head;curNode!=null ;curNode=curNode.next ) {
+            System.out.print(curNode.data+" ");
+          }
+          System.out.println();
+        }
+      }
+      public static void main(String[] args) {
+        LinkedListGenerics<Integer> linkedlist=new LinkedListGenerics<>();
+        linkedlist.addNode(10);
+        linkedlist.addNode(20);
+        linkedlist.addNode(30);
+        linkedlist.addNode(40);
+        linkedlist.traverseList();
+        linkedlist.deleteNode();
+        linkedlist.traverseList();
+      }
+}
