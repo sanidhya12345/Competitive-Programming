1bbd9edb7b2535d87b2713b803276af099c20d6e
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-02-03 18:36:30 +0530

    odd even linked list

---
+++ CB_LL_ASSIGNMENT/src/LinkedListOddEven.java
@@ -0,0 +1,272 @@
+import java.util.Scanner;
+
+public class LinkedListOddEven {
+    private class Node {
+        int data;
+        Node next;
+
+        Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+    private int size;
+
+
+
+    // O(1)
+    public int size() {
+        return this.size;
+    }
+
+    // O(1)
+    public boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    // O(1)
+    public int getFirst() throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("List is empty.");
+        }
+
+        return this.head.data;
+    }
+
+    // O(1)
+    public int getLast() throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("List is empty.");
+        }
+
+        return this.tail.data;
+    }
+
+    // O(N)
+    public int getAt(int idx) throws Exception {
+        Node temp = this.getNodeAt(idx);
+        return temp.data;
+    }
+
+    // O(N)
+    private Node getNodeAt(int idx) throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("List is empty");
+        }
+
+        if (idx < 0 || idx >= this.size()) {
+            throw new Exception("Invalid arguments");
+        }
+
+        Node retVal = this.head;
+        for (int i = 0; i < idx; i++) {
+            retVal = retVal.next;
+        }
+
+        return retVal;
+    }
+
+    // O(1)
+    public void addFirst(int data) {
+        Node node = new Node(data, this.head);
+
+        if (this.size() == 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.head = node;
+        }
+
+        this.size++;
+    }
+
+    // O(1)
+    public void addLast(int data) {
+        Node node = new Node(data, null);
+
+        if (this.size() == 0) {
+            this.head = node;
+            this.tail = node;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+
+        this.size++;
+    }
+
+    // O(n)
+    public void addAt(int idx, int data) throws Exception {
+        if (idx < 0 || idx > this.size()) {
+            throw new Exception("Invalid arguments");
+        }
+
+        if (idx == 0) {
+            this.addFirst(data);
+        } else if (idx == this.size()) {
+            this.addLast(data);
+        } else {
+            Node nm1 = this.getNodeAt(idx - 1);
+            Node n = nm1.next;
+
+            Node node = new Node(data, n);
+            nm1.next = node;
+
+            this.size++;
+        }
+    }
+
+    // O(1)
+    public int removeFirst() throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("List is empty");
+        }
+
+        int retVal = this.head.data;
+
+        if (this.size() == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            this.head = this.head.next;
+        }
+
+        this.size--;
+        return retVal;
+    }
+
+    // O(n)
+    public int removeLast() throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("List is empty");
+        }
+
+        int retVal = this.tail.data;
+
+        if (this.size() == 1) {
+            this.head = null;
+            this.tail = null;
+        } else {
+            Node sm2 = this.getNodeAt(this.size() - 2);
+            sm2.next = null;
+            this.tail = sm2;
+        }
+
+        this.size--;
+        return retVal;
+    }
+
+    // O(n)
+    public int removeAt(int idx) throws Exception {
+        if (this.isEmpty()) {
+            throw new Exception("List is empty");
+        }
+
+        if (idx < 0 || idx >= this.size()) {
+            throw new Exception("Invalid arguments");
+        }
+
+        if (idx == 0) {
+            return this.removeFirst();
+        } else if (idx == this.size() - 1) {
+            return this.removeLast();
+        } else {
+            Node nm1 = this.getNodeAt(idx - 1);
+            Node n = nm1.next;
+            Node np1 = n.next;
+
+            nm1.next = np1;
+            this.size--;
+
+            return n.data;
+        }
+    }
+
+    // O(n)
+    public void display() {
+        Node node = this.head;
+
+        while (node != null) {
+            System.out.print(node.data + " ");
+            node = node.next;
+        }
+
+        //System.out.println("END");
+    }
+    public void reverse_DI() {
+        Node curr = this.head,prev=null;
+        while(curr!=null)
+        {
+            Node temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        this.head = prev;
+        this.tail = this.head;
+    }
+    public void partition(){
+        // Write your code here
+        Node temp = this.head;
+        if(temp==null || temp.next==null)
+            return;
+        // System.out.println(temp.data);
+        Node odd=null,eve=null,od=null,even=null;
+        while(temp!=null)
+        {
+            if((temp.data)%2==0)
+            {
+                if(eve == null)
+                {
+                    eve = new Node(temp.data,null);
+                    even = eve;
+                }
+
+                else
+                {
+                    eve.next = new Node(temp.data,null);
+                    eve = eve.next;
+                }
+                // temp=temp.next;
+            }
+            else{
+                if(od==null)
+                {
+                    od = new Node(temp.data,null);
+                    odd = od;
+                }
+                else{
+                    od.next = new Node(temp.data,null);
+                    od = od.next;
+                }
+            }
+            temp=temp.next;
+        }
+        if(od!=null)
+            od.next = even;
+        else{
+            odd = even;
+        }
+        this.head = odd;
+
+    }
+}
+class Main{
+    public static void main(String[] args) throws Exception {
+
+        Scanner scn = new Scanner(System.in);
+        LinkedListOddEven list = new LinkedListOddEven();
+        int n = scn.nextInt();
+
+        for (int j = 0; j < n; j++) {
+            int item = scn.nextInt();
+            list.addLast(item);
+        }
+
+        list.partition();
+        list.display();
+    }
+}
