b7355442eb36aa27b9f35cd1c379666088dccd72
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-02-12 12:36:00 +0530

    create tree using preorder and inorder

---
+++ Cb_trees/src/Create_tree_Pre_and_in.java
@@ -0,0 +1,102 @@
+/*import java.util.*;
+public class Main {
+
+	static Scanner scn = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		Main m = new Main();
+		int[] pre = takeInput();
+		int[] in = takeInput();
+		BinaryTree bt = m.new BinaryTree(pre, in);
+		bt.display();
+	}
+
+	public static int[] takeInput() {
+		int n = scn.nextInt();
+
+		int[] rv = new int[n];
+		for (int i = 0; i < rv.length; i++) {
+			rv[i] = scn.nextInt();
+		}
+
+		return rv;
+	}
+
+	private class BinaryTree {
+		private class Node {
+			int data;
+			Node left;
+			Node right;
+		}
+
+		private Node root;
+		private int size;
+
+		public BinaryTree(int[] pre, int[] in) {
+			this.root = this.construct(pre, 0, pre.length - 1, in, 0, in.length - 1);
+		}
+
+		private Node construct(int[] pre, int plo, int phi, int[] in, int ilo, int ihi) {
+
+			// write your code here
+			if (plo > phi || ilo > ihi)
+			return null;
+
+		Node nn = new Node();
+		nn.data = pre[plo];
+
+		int si = -1;
+		int nel = 0;
+
+		for (int i = ilo; i <= ihi; i++) {
+
+			if (in[i] == pre[plo]) {
+				si = i;
+				break;
+			}
+
+			nel++;
+		}
+
+		nn.left = construct(pre, plo + 1, plo + nel, in, ilo, si - 1);
+		nn.right = construct(pre, plo + nel + 1, phi, in, si + 1, ihi);
+
+		return nn;
+
+		}
+
+		public void display() {
+			this.display(this.root);
+		}
+
+		private void display(Node node) {
+			if (node == null) {
+				return;
+			}
+
+			String str = "";
+
+			if (node.left != null) {
+				str += node.left.data;
+			} else {
+				str += "END";
+			}
+
+			str += " => " + node.data + " <= ";
+
+			if (node.right != null) {
+				str += node.right.data;
+			} else {
+				str += "END";
+			}
+
+			System.out.println(str);
+
+			this.display(node.left);
+			this.display(node.right);
+		}
+
+	}
+
+}
+*/
