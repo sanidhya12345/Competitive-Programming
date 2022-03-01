90b9f7de8f5ac45286be9fe4a380a7f37496d3b7
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-03-01 10:06:36 +0530

    Inorder traversal without using recursion
    Signed-off-by: Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>

---
+++ Trees/src/InorderTraversalWithoutRecursion.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+public class InorderTraversalWithoutRecursion {
+    public static TreeNode root;
+    private static class TreeNode{
+        private TreeNode left;
+        private TreeNode right;
+        private int data;
+
+        public TreeNode(int data){
+            this.data=data;
+        }
+    }
+    public TreeNode createBinaryTree(){
+        TreeNode first= new TreeNode(1);
+        TreeNode second= new TreeNode(2);
+        TreeNode third= new TreeNode(3);
+        TreeNode fourth= new TreeNode(4);
+        TreeNode fifth= new TreeNode(5);
+        TreeNode sixth= new TreeNode(6);
+        TreeNode seventh= new TreeNode(7);
+        root=first;
+        first.left=second;
+        first.right=third;
+        second.left=fourth;
+        second.right=fifth;
+        third.left=sixth;
+        third.right=seventh;
+        return root;
+    }
+    public void inorder(TreeNode root){
+        if (root==null){
+            return;
+        }
+        Stack<TreeNode> s=new Stack<>();
+        TreeNode curr=root;
+        while(curr!=null || s.size()>0){
+            while(curr!=null){
+                s.push(curr);
+                curr=curr.left;
+            }
+            curr=s.pop();
+            System.out.println(curr.data);
+            curr=curr.right;
+        }
+    }
+    public static void main(String[] args) {
+        InorderTraversalWithoutRecursion tree=new InorderTraversalWithoutRecursion();
+        TreeNode root=tree.createBinaryTree();
+        tree.inorder(root);
+
+    }
+}
