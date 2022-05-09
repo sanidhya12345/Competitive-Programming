6bec56839c7685e917517e9faaf4803f6c058190
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-05-09 09:33:27 +0530

    Graph qps
    Signed-off-by: Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>

--- GrpahCB/src/Graph.java
+++ GrpahCB/src/Graph.java
@@ -1,53 +1,67 @@
 import java.util.HashMap;
 
 public class Graph {
-    HashMap<Integer,HashMap<Integer,Integer>>map;
-    public Graph(int v){
-        map=new HashMap<>();
-        for(int i=1;i<=v;i++){
-            map.put(i,new HashMap<>());
+    HashMap<Integer, HashMap<Integer, Integer>> map;
+
+    public Graph(int v) {
+        // TODO Auto-generated constructor stub
+        map = new HashMap<>();
+
+        for (int i = 1; i <= v; i++) {
+            map.put(i, new HashMap<>());
         }
     }
-    public void addEdge(int v1,int v2,int cost){
-        map.get(v1).put(v2,cost);
-        map.get(v2).put(v1,cost);
+
+    public void addedge(int v1, int v2, int cost) {
+        map.get(v1).put(v2, cost);
+        map.get(v2).put(v1, cost);
+
     }
-    public int noofedge(){
-        int count=0;
-        for(int key:map.keySet()){
-            count+=map.get(key).size();
+
+    public int noofedge() {
+
+        int ans = 0;
+        for (int key : map.keySet()) {
+            ans = ans + map.get(key).size();
         }
-        return count/2;
+        return ans / 2;
     }
-    public boolean containsEdge(int v1,int v2){
+
+    public boolean ContainsEdge(int v1, int v2) {
         return map.get(v1).containsKey(v2) && map.get(v2).containsKey(v1);
     }
-    public void removeVertex(int v){
-        for(int key:map.get(v).keySet()){
+
+    public void removeEdge(int v1, int v2) {
+        if (ContainsEdge(v1, v2)) {
+            map.get(v1).remove(v2);
+            map.get(v2).remove(v1);
+
+        }
+    }
+
+    public void removeVertx(int v) {
+        for (int key : map.get(v).keySet()) {
             map.get(key).remove(v);
+
         }
         map.remove(v);
     }
-    public void display(){
-        for(int key:map.keySet()){
-            System.out.print(key+" "+map.get(key));
-        }
-    }
-    public void removeEdge(int v1,int v2){
-        if(containsEdge(v1,v2)){
-            map.get(v1).remove(v2);
-            map.get(v2).remove(v1);
+
+    public void Display() {
+        for (int key : map.keySet()) {
+            System.out.println(key + " " + map.get(key));
         }
     }
     public static void main(String[] args) {
-        Graph g=new Graph(7);
-        g.addEdge(1,4,5);
-        g.addEdge(1,2,10);
-        g.addEdge(2,3,7);
-        g.addEdge(3,4,6);
-        g.addEdge(8,7,12);
-        g.addEdge(6,7,9);
-        g.addEdge(19,78,5);
-        g.display();
+        Graph g = new Graph(7);
+        g.addedge(1, 4, 5);
+        g.addedge(1, 2, 10);
+        g.addedge(2, 3, 7);
+        g.addedge(3, 4, 2);
+        g.addedge(4, 5, 4);
+        g.addedge(5, 6, 3);
+        g.addedge(5, 7, 2);
+        g.addedge(6, 7, 7);
+        g.Display();
     }
 }
---
+++ GrpahCB/src/Graph_Practice.java
@@ -0,0 +1,100 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class Graph_Practice {
+    HashMap<Integer, HashMap<Integer,Integer>>map;
+    public Graph_Practice(int v){
+        map=new HashMap<>();
+        for(int i=1;i<=v;i++){
+            map.put(i,new HashMap<>());
+        }
+    }
+    public void addEdge(int v1,int v2,int cost){
+        map.get(v1).put(v2,cost);
+        map.get(v2).put(v1,cost);
+    }
+    public boolean hasPath(int source, int destination, HashSet<Integer> visited){
+        if (source==destination) return true;
+        visited.add(source);
+        for(int key:map.get(source).keySet()){
+            if(!visited.contains(key)){
+                boolean ispath=hasPath(key,destination,visited);
+                if(ispath) return true;
+            }
+        }
+        visited.remove(source);
+        return false;
+    }
+    public void PrintAllPath(int source,int destination,HashSet<Integer> visited,String s){
+        if(source==destination) {
+            System.out.println(s+destination);
+            visited.remove(source);
+            return;
+        }
+        visited.add(source);
+        for(int key:map.get(source).keySet()){
+            if(!visited.contains(key)){
+                PrintAllPath(key,destination,visited,s+source);
+            }
+        }
+        visited.remove(source);
+    }
+    public boolean BFS(int source,int destination){
+        LinkedList<Integer> queue=new LinkedList<>();
+        HashSet<Integer> visited=new HashSet<>();
+        queue.add(source);
+        while (!queue.isEmpty()){
+            int rv=queue.remove();
+            if(visited.contains(rv)){
+                continue;
+            }
+            visited.add(rv);
+            if (rv==destination){
+                return true;
+            }
+            for (int key:map.get(rv).keySet()){
+                if(!visited.contains(key)){
+                    queue.add(key);
+                }
+            }
+        }
+        return false;
+    }
+    public boolean DFS(int source,int destination){
+        Stack<Integer> stack=new Stack<>();
+        HashSet<Integer> visited=new HashSet<>();
+        stack.push(source);
+        while(!stack.isEmpty()){
+            int rv=stack.pop();
+            if(visited.contains(rv)){
+                continue;
+            }
+            visited.add(rv);
+            if (rv==destination){
+                return true;
+            }
+
+            for (int key:map.get(source).keySet()){
+                if (!visited.contains(key)){
+                    stack.push(key);
+                }
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        Graph_Practice graph=new Graph_Practice(7);
+        graph.addEdge(1, 4, 5);
+        graph.addEdge(1, 2, 10);
+        graph.addEdge(2, 3, 7);
+        graph.addEdge(3, 4, 2);
+        graph.addEdge(4, 5, 4);
+        graph.addEdge(5, 6, 3);
+        graph.addEdge(5, 7, 2);
+        graph.addEdge(6, 7, 7);
+        graph.PrintAllPath(1,6,new HashSet<>(),"");
+        System.out.println(graph.BFS(1,5));
+    }
+}
