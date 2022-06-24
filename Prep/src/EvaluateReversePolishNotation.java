835fdfb80f67847514ee53312ade630e4408f9b9
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-06-24 18:26:20 +0530

    evaluate reverse polish notation

---
+++ Prep/src/EvaluateReversePolishNotation.java
@@ -0,0 +1,40 @@
+import java.util.Stack;
+
+public class EvaluateReversePolishNotation {
+    private static int evalRPN(String [] tokens){
+        int ans=0;
+        String operators="+-*/";
+        Stack<String> stack=new Stack<>();
+        for(String t:tokens){
+            if(!operators.contains(t)){
+                stack.push(t);
+            }
+            else{
+                int a=Integer.valueOf(stack.pop());
+                int b=Integer.valueOf(stack.pop());
+                int index=operators.indexOf(t);
+                switch (index){
+                    case 0:
+                        stack.push(String.valueOf(a+b));
+                        break;
+                    case 1:
+                        stack.push(String.valueOf(b-a));
+                        break;
+                    case 2:
+                        stack.push(String.valueOf(b*a));
+                        break;
+                    case 3:
+                        stack.push(String.valueOf(b/a));
+                        break;
+                }
+            }
+        }
+        ans=Integer.valueOf(stack.pop());
+        return ans;
+    }
+    public static void main(String[] args) {
+        String [] tokens={"2","1","+","3","*"};
+        int ans=evalRPN(tokens);
+        System.out.println(ans);
+    }
+}
