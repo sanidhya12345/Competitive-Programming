3bac268f10aa717d04c105663e0fad447bc9c6e9
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-06-25 17:06:44 +0530

    median of two sorted arrays

---
+++ Prep/src/MedianOfTwoSortedArrays.java
@@ -0,0 +1,51 @@
+public class MedianOfTwoSortedArrays {
+    public static double findMedian(int [] A,int [] B){
+        int m=A.length;
+        int n=B.length;
+        if((m+n)%2!=0){
+            return (double) findk(A,B,(m+n)/2,0,m-1,0,n-1);
+        }
+        else{
+            return (findk(A,B,(m+n)/2,0,m-1,0,n-1)+findk(A,B,(m+n)/2-1,0,m-1,0,n-1))*0.5;
+        }
+    }
+    private static int findk(int [] A,int [] B,int k,int aStart,int aEnd,int bStart,int bEnd){
+        int aLen=aEnd-aStart+1;
+        int bLen=bEnd-bStart+1;
+
+        //handle special cases
+
+        if(aLen==0){
+            return B[bStart+k];
+        }
+        if(bLen==0){
+            return A[aStart+k];
+        }
+        if(k==0){
+            return Math.max(A[aStart], B[bStart]);
+        }
+
+        int aMid=aLen*k/(aLen+bLen);
+        int bMid=bLen*k/(aLen+bLen);
+
+        aMid=aMid+aStart;
+        bMid=bMid+bStart;
+
+        if(A[aMid]>B[bMid]){
+            k=k-(bMid-bStart+1);
+            aEnd=aMid;
+            bStart=bMid+1;
+        }
+        else{
+            k=k-(aMid-aStart+1);
+            bEnd=bMid;
+            aStart=aMid+1;
+        }
+        return findk(A,B,k,aStart,aEnd,bStart,bEnd);
+    }
+    public static void main(String[] args) {
+        int [] A={1,2,3,4,5};
+        int [] B={8,9,12,34,35};
+        System.out.println(findMedian(A,B));
+    }
+}
