648654c12d9e5ae8da884b203322b26b350ea237
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-06-25 17:44:07 +0530

    Time: 2 ms (100.00%), Space: 42.6 MB (94.02%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i=0;
+        int j=0;
+        int startIndex=-1;
+        int iIndex=-1;
+        while(i<s.length()){
+            if(j<p.length() &&(p.charAt(j)=='?'||p.charAt(j)==s.charAt(i))){
+                i++;
+                j++;
+            } else if (j<p.length() && p.charAt(j)=='*') {
+                startIndex=j;
+                iIndex=i;
+                j++;
+            } else if (startIndex!=-1) {
+                j=startIndex+1;
+                i=iIndex+1;
+                iIndex++;
+            }
+            else{
+                return false;
+            }
+        }
+        while(j<p.length() && p.charAt(j)=='*'){
+            j++;
+        }
+        return j==p.length();
+    }
+}
