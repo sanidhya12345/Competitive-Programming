83f8fb5018e8d8a008c82424f16f70881e03d4fe
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-08-04 19:25:37 +0530

    Time: 706 ms (14.98%), Space: 178.2 MB (5.72%) - LeetHub

---
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            if(!set.contains(nums[i]-1)){
+                
+                int j=nums[i];
+                while(set.contains(j)){
+                    j++;
+                }
+                if(ans<j-nums[i]){
+                    ans=j-nums[i];
+                }
+            }
+        }
+        return ans;
+    }
+}
