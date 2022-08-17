8a46b2aa1ca3f37aca5e4af7be648dfb889061fe
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-08-17 18:31:02 +0530

    Time: 0 ms (100.00%), Space: 61.1 MB (10.61%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,21 +1,44 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-         HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i:nums){
-            if (!map.containsKey(i)){
-                map.put(i,1);
+    //Using HashMap.
+    // public int singleNonDuplicate(int[] nums) {
+    //      HashMap<Integer,Integer> map=new HashMap<>();
+    //     for(int i:nums){
+    //         if (!map.containsKey(i)){
+    //             map.put(i,1);
+    //         }
+    //         else{
+    //             map.put(i,map.get(i)+1);
+    //         }
+    //     }
+    //     int ans=0;
+    //     for(int i:map.keySet()){
+    //         if(map.get(i)==1){
+    //             ans=i;
+    //             break;
+    //         }
+    //     }
+    //     return ans;
+    // }
+    public int singleNonDuplicate(int [] nums){
+        return binarySearchHelper(nums);
+    }
+    private int binarySearchHelper(int [] nums){
+        int low=0;
+        int high=nums.length-1;
+        while(low<high){
+            int mid=(low+high)/2;
+            
+            if(nums[mid]==nums[mid+1]){
+                mid=mid-1;
+            }
+            if((mid-low+1)%2!=0){
+                high=mid;
             }
             else{
-                map.put(i,map.get(i)+1);
+                low=mid+1;
             }
+          
         }
-        int ans=0;
-        for(int i:map.keySet()){
-            if(map.get(i)==1){
-                ans=i;
-                break;
-            }
-        }
-        return ans;
+        return nums[low];
     }
 }
