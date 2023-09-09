37748e4d03818cff444e456ab83353680089b578
Sanidhya Varshney <sanidhya.varshney_cs19@gla.ac.in>
2023-09-09 17:15:58 +0530

    Added solution - LeetHub

---
+++ Key Pair - GFG/key-pair.java
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine().trim());
+        while (tc-- > 0) {
+            String[] inputLine;
+            inputLine = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(inputLine[0]);
+            int x = Integer.parseInt(inputLine[1]);
+            int[] arr = new int[n];
+            inputLine = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            boolean ans = new Solution().hasArrayTwoCandidates(arr, n, x);
+            System.out.println(ans ? "Yes" : "No");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Solution {
+    boolean hasArrayTwoCandidates(int arr[], int n, int sum) {
+       HashMap<Integer,Boolean> map=new HashMap<>();
+       HashSet<Integer> s = new HashSet<Integer>();
+        for (int i = 0; i < arr.length; ++i) {
+            int temp = sum - arr[i];
+            if (s.contains(temp)) {
+                return true;
+            }
+            s.add(arr[i]);
+        }
+        return false;
+    }
+}
